0f249d282970b64916f42d0fffde3663a1e3a636
Evgenii Poperezhai <eapdob@gmail.com>
2023-11-28 00:32:09 +0200

    - added Languages -- added Language front end controller for saving in session language value -- added Language route for ajax request for saving session language

---
+++ app/Http/Controllers/Frontend/LanguageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LanguageController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        session(['language' => $request->language_code]);
+
+        return response(['status' => 'success']);
+    }
+}
