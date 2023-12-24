153b1d9ef5f0b26bd55bf870dd8afb21c6839225
Evgenii Poperezhai <eapdob@gmail.com>
2023-12-24 21:11:59 +0200

    - added Details -- added Comment model and migrations -- edited news-details for adding comment -- added handleComment method -- edited docker-compose.yml

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,5 @@ require __DIR__ . '/auth.php';
 Route::get('language', LanguageController::class)->name('language');
 
 Route::get('news-details/{slug}', [HomeController::class, 'showNews'])->name('news-details');
+
+Route::post('news-comment', [HomeController::class, 'handleComment'])->name('news-comment');
